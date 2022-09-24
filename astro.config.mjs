538da60a69e15a742f55c9646b43875a15fc7270
Temmy Lizzy <temmylizzy59@gmail.com>
2022-09-24 14:18:22 +0000

    added turbolinks for reactive navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import turbolinks from'@astrojs/turbolinks';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), turbolinks()]
 });
